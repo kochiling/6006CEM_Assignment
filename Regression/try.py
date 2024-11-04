b5b2dd3a419753b8ec1a91a667291d825620fc60
kochiling <112928080+kochiling@users.noreply.github.com>
2024-11-04 23:29:59 +0800

    Create try.py

---
+++ Regression/try.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn import metrics
